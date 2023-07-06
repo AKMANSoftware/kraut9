2847687f33df075a39ba235eed08c3b70769f5cc
Ali Hussnain Mi6 <haideraqeel86@hotmail.com>
2023-07-06 11:40:06 +0500

    s

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 // import button from button comonents
+import { useLocation } from "react-router-dom";
 import { PrimaryButton } from "./Buttons";
+import { cn } from "../lib/utils";
 // Header Compponent
 export default function Header() {
+    // const { pathname } = useLocation()
     return (
 
         <header className="xl:mx-auto md:px-2 px-4 flex  justify-between align-middle max-w-screen-xl sticky py-5 top-0 bg-white z-50">
-            <div className="md:flex hidden text-black font-normal navbar  text-base lg:gap-x-10  md:gap-x-2 items-center ">
-                <a href="/collections">
+            <div className="md:flex hidden text-black font-normal navbar text-base lg:gap-x-10 md:gap-x-2 items-center ">
+                <a href="/collections" className={cn(
+                    // pathname === "/collections" && "text-primary"
+                )}>
                     Collections
                 </a>
                 <a href="/use-cases">
--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,92 +1,95 @@
 import NftPostComponent from "../components/NftPostComponent";
 import { PrimaryButton } from "../components/Buttons";
+import Layout from "../components/layout";
 
 // CollectionPage component
 export default function CollectionPage() {
     return (
-        <div className="max-w-screen-xl xl:mx-auto  mx-4 py-5 md:py-[90px]">
-            {/* div with heading and description */}
-            <div className="md:flex md:flex-row flex-col justify-between items-center md:py-0 py-5">
-                <div className="lg:text-[3.25rem] text-[2rem] font-extrabold text-secondary">
-                    Collections
+        <Layout>
+            <div className="max-w-screen-xl xl:mx-auto  mx-4 py-5 md:py-[90px]">
+                {/* div with heading and description */}
+                <div className="md:flex md:flex-row flex-col justify-between items-center md:py-0 py-5">
+                    <div className="lg:text-[3.25rem] text-[2rem] font-extrabold text-secondary">
+                        Collections
+                    </div>
+                    <div className="font-inter lg:text-base text-sm text-secondary font-normal md:w-1/2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore consectetur adipiscing elit, sed do
+                    </div>
                 </div>
-                <div className="font-inter lg:text-base text-sm text-secondary font-normal md:w-1/2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore consectetur adipiscing elit, sed do
+
+                {/* First collection div */}
+                {/* Render CollectionComponent */}
+                <div className="py-[50px]">
+                    <NftPostComponent
+                        mediaVariant="video"
+                        src={'/images/mixkit-a-man-with-a-rainbow-flag-1349-medium.mp4'}
+                        heading="Mini Robots"
+                        content={
+                            <>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </>
+                        }
+                        button={
+                            // Render PrimaryButton
+                            <PrimaryButton>
+                                JOIN WAITLIST
+                            </PrimaryButton>
+                        }
+                    />
+                </div>
+
+
+                {/* Heading for upcoming collections */}
+                <div className="text-2xl font-extrabold text-secondary py-[30px] flex items-center justify-center">
+                    Upcoming Collections
+                </div>
+                <div className="space-y-[30px]">
+
+                    {/* Second collection div */}
+                    {/* Render CollectionComponent */}
+                    <NftPostComponent
+                        mediaVariant="image"
+                        src={'/images/blackimage-for-componentpage.svg'}
+                        heading="Collection Name"
+                        content={
+                            <>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </>
+                        }
+                        button={
+                            // Render PrimaryButton with custom className
+                            <PrimaryButton className="text-tersioary from-secondary to-secondary">
+                                COMING SOON
+                            </PrimaryButton>
+                        }
+                    />
+
+                    {/* Third collection div */}
+                    {/* Render CollectionComponent */}
+                    <NftPostComponent
+                        mediaVariant="image"
+                        src={'/images/blackimage-for-componentpage.svg'}
+                        heading="Collection Name"
+                        content={
+                            <>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </>
+                        }
+                        button={
+                            // Render PrimaryButton with custom className
+                            <PrimaryButton className="text-tersioary from-secondary to-secondary">
+                                COMING SOON
+                            </PrimaryButton>
+                        }
+                    />
                 </div>
             </div>
-
-            {/* First collection div */}
-            {/* Render CollectionComponent */}
-            <div className="py-[50px]">
-                <NftPostComponent
-                    mediaVariant="video"
-                    src={'/images/mixkit-a-man-with-a-rainbow-flag-1349-medium.mp4'}
-                    heading="Mini Robots"
-                    content={
-                        <>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </>
-                    }
-                    button={
-                        // Render PrimaryButton
-                        <PrimaryButton>
-                            JOIN WAITLIST
-                        </PrimaryButton>
-                    }
-                />
-            </div>
-
-
-            {/* Heading for upcoming collections */}
-            <div className="text-2xl font-extrabold text-secondary py-[30px] flex items-center justify-center">
-                Upcoming Collections
-            </div>
-            <div className="space-y-[30px]">
-
-                {/* Second collection div */}
-                {/* Render CollectionComponent */}
-                <NftPostComponent
-                    mediaVariant="image"
-                    src={'/images/blackimage-for-componentpage.svg'}
-                    heading="Collection Name"
-                    content={
-                        <>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </>
-                    }
-                    button={
-                        // Render PrimaryButton with custom className
-                        <PrimaryButton className="text-tersioary from-secondary to-secondary">
-                            COMING SOON
-                        </PrimaryButton>
-                    }
-                />
-
-                {/* Third collection div */}
-                {/* Render CollectionComponent */}
-                <NftPostComponent
-                    mediaVariant="image"
-                    src={'/images/blackimage-for-componentpage.svg'}
-                    heading="Collection Name"
-                    content={
-                        <>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </>
-                    }
-                    button={
-                        // Render PrimaryButton with custom className
-                        <PrimaryButton className="text-tersioary from-secondary to-secondary">
-                            COMING SOON
-                        </PrimaryButton>
-                    }
-                />
-            </div>
-        </div>
+        </Layout>
     );
 }
