a84984d6ceba924eb9778c58794417ba38bbd8cf
Ali Hussnain Mi6 <haideraqeel86@hotmail.com>
2023-07-10 18:04:43 +0500

    float animation added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,97 +13,99 @@ export default function Header() {
     };
 
     return (
-        <header className="xl:mx-auto md:px-2 px-4 flex justify-between align-middle max-w-screen-xl sticky py-5 top-0 bg-white z-40">
-            <div className="lg:hidden  flex items-center md:w-auto w-[70px]">
-                <button onClick={toggleMenu}>
-                    <img src="/icons/bar_menue.svg" />
-                </button>
+        <header className="sticky py-5 top-0 bg-white z-40">
+            <div className="xl:mx-auto md:px-2 px-4 flex justify-between align-middle max-w-screen-xl">
+                <div className="lg:hidden  flex items-center md:w-auto w-[70px]">
+                    <button onClick={toggleMenu}>
+                        <img src="/icons/bar_menue.svg" />
+                    </button>
 
-            </div>
-            <div className="lg:flex hidden  text-black font-normal text-base gap-x-10 items-center">
-                <a
-                    href="/collections"
-                    className={cn(
-                        pathname === "/collections" && "text-primary",
-                        "hover:text-primary"
+                </div>
+                <div className="lg:flex hidden  text-black font-normal text-base gap-x-10 items-center">
+                    <a
+                        href="/collections"
+                        className={cn(
+                            pathname === "/collections" && "text-primary",
+                            "hover:text-primary"
+                        )}
+                    >
+                        Collections
+                    </a>
+
+                    <a
+                        href="/use-cases"
+                        className={cn(
+                            pathname === "/use-cases" && "text-primary",
+                            "hover:text-primary"
+                        )}
+                    >
+                        Use Cases
+                    </a>
+                    <a
+                        href="/team"
+                        className={cn(
+                            pathname === "/team" && "text-primary",
+                            "hover:text-primary"
+                        )}
+                    >
+                        Team
+                    </a>
+
+                </div>
+                {/* div for mobile responsive */}
+                <div className="absolute z-50 bg-white w-[calc(100%-20px)]">
+                    {isMenuOpen && (
+                        <div className="lg:hidden flex flex-col text-black font-normal text-base space-y-10 pb-10">
+                            <button onClick={toggleMenu} className="absolute">
+                                <i className="fa-solid fa-xmark"></i>
+                            </button>
+                            <a
+                                href="/collections"
+                                className={cn(
+                                    pathname === "/collections" && "text-primary",
+                                    "hover:text-primary"
+                                )}
+                            >
+                                Collections
+                            </a>
+
+                            <a
+                                href="/use-cases"
+                                className={cn(
+                                    pathname === "/use-cases" && "text-primary",
+                                    "hover:text-primary"
+                                )}
+                            >
+                                Use Cases
+                            </a>
+                            <a
+                                href="/team"
+                                className={cn(
+                                    pathname === "/team" && "text-primary",
+                                    "hover:text-primary"
+                                )}
+                            >
+                                Team
+                            </a>
+                            <PrimaryButton className="">
+                                MINT
+                            </PrimaryButton>
+                        </div>
                     )}
-                >
-                    Collections
-                </a>
-
-                <a
-                    href="/use-cases"
-                    className={cn(
-                        pathname === "/use-cases" && "text-primary",
-                        "hover:text-primary"
-                    )}
-                >
-                    Use Cases
-                </a>
-                <a
-                    href="/team"
-                    className={cn(
-                        pathname === "/team" && "text-primary",
-                        "hover:text-primary"
-                    )}
-                >
-                    Team
-                </a>
-
-            </div>
-            {/* div for mobile responsive */}
-            <div className="absolute z-50 bg-white w-[calc(100%-20px)]">
-                {isMenuOpen && (
-                    <div className="lg:hidden flex flex-col text-black font-normal text-base space-y-10 pb-10">
-                        <button onClick={toggleMenu} className="absolute">
-                            <i className="fa-solid fa-xmark"></i>
-                        </button>
-                        <a
-                            href="/collections"
-                            className={cn(
-                                pathname === "/collections" && "text-primary",
-                                "hover:text-primary"
-                            )}
-                        >
-                            Collections
-                        </a>
-
-                        <a
-                            href="/use-cases"
-                            className={cn(
-                                pathname === "/use-cases" && "text-primary",
-                                "hover:text-primary"
-                            )}
-                        >
-                            Use Cases
-                        </a>
-                        <a
-                            href="/team"
-                            className={cn(
-                                pathname === "/team" && "text-primary",
-                                "hover:text-primary"
-                            )}
-                        >
-                            Team
-                        </a>
-                        <PrimaryButton className="">
-                            MINT
-                        </PrimaryButton>
-                    </div>
-                )}
-            </div>
-            <div className="flex items-center xl:w-[27%] lg:w-[32%]">
-                <a href="/">
-                    <img
-                        src="/images/logo_sm.svg"
-                        className="md:w-[170px] md:h-[105px] w-[129px] h-[80px]"
-                    />
-                </a>
-            </div>
-            <div className="flex items-center">
-                <a href="/mint">
-                    <PrimaryButton className="w-auto">MINT</PrimaryButton>
-                </a>
+                </div>
+                <div className="flex items-center xl:w-[27%] lg:w-[32%]">
+                    <a href="/">
+                        <img
+                            src="/images/logo_sm.svg"
+                            className="md:w-[170px] md:h-[105px] w-[129px] h-[80px]"
+                        />
+                    </a>
+                </div>
+                <div className="flex items-center">
+                    <a href="/mint">
+                        <PrimaryButton className="w-auto">MINT</PrimaryButton>
+                    </a>
+                </div>
             </div>
         </header>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-    theme: {
+  theme: {
     extend: {
       // define all colors
-      colors:{
-       primary: '#28C6F3',
-       secondary:'#27282A',
-       tersioary:'#FDCB2A',
-       tersioarysolid:'#FFCB00',
-       halfSecondary:'#27282A80',
-       lightwhite:'#c5e5f0'
+      colors: {
+        primary: '#28C6F3',
+        secondary: '#27282A',
+        tersioary: '#FDCB2A',
+        tersioarysolid: '#FFCB00',
+        halfSecondary: '#27282A80',
+        lightwhite: '#c5e5f0'
+      },
+      keyframes: {
+        motion1: {
+          "0%, 100%": {
+            top: "0px",
+            left: "0px"
+          },
+          "50%": {
+            top: "100%",
+            left: "100%"
+          },
+        },
+        motion2: {
+          "0%, 100%": {
+            top: "0px",
+            right: "0px"
+          },
+          "50%": {
+            top: "100%",
+            right: "100%"
+          },
+        }
+      },
+      animation: {
+        "motion1": "motion1 15s linear infinite",
+        "motion2": "motion2 15s linear infinite"
       }
     },
   },
