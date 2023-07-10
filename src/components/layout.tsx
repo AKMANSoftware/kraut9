4782429c68e9aa60f385958502c7306c1c6003dc
Ali Hussnain Mi6 <haideraqeel86@hotmail.com>
2023-07-10 18:05:50 +0500

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,25 @@
 import Footer from './Footer';
 import Header from './Header';
 
-type Props ={
+type Props = {
   children: React.ReactNode;
+  showAnimation?: boolean;
 }
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children, showAnimation = true }: Props) {
   return (
     <>
       <Header />
-      {children}
+      <main>
+        {children}
+        {
+          showAnimation &&
+          <>
+            <div className="w-[396px] h-[396px] aspect-square rounded-full bg-tersioary/40 blur-[100px] -translate-x-1/2 -translate-y-1/2 fixed animate-motion1 z-[60]" />
+            <div className="w-[396px] h-[396px] aspect-square rounded-full bg-tersioary/40 blur-[100px] delay-[5s] translate-x-1/2 -translate-y-1/2 fixed animate-motion2 z-[60]" />
+          </>
+        }
+      </main>
       <Footer />
     </>
   );
